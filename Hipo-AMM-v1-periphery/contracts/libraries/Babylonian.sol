d7916b1bec5f13610759070a1845d9029ca5dd96
A1RPotter <92673692+A1RPotter@users.noreply.github.com>
2021-11-07 17:03:41 +0800

    Update Babylonian.sol

--- Hipo-AMM-v1-periphery/contracts/libraries/Babylonian.sol
+++ Hipo-AMM-v1-periphery/contracts/libraries/Babylonian.sol
@@ -1,85 +1,53 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.5.0;
 
-library BitMath {
-    // returns the 0 indexed position of the most significant bit of the input x
-    // s.t. x >= 2**msb and x < 2**(msb+1)
-    function mostSignificantBit(uint256 x) internal pure returns (uint8 r) {
-        require(x > 0, 'BitMath::mostSignificantBit: zero');
+pragma solidity >=0.4.0;
 
-        if (x >= 0x100000000000000000000000000000000) {
-            x >>= 128;
-            r += 128;
+// computes square roots using the babylonian method
+// https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
+library Babylonian {
+    // credit for this implementation goes to
+    // https://github.com/abdk-consulting/abdk-libraries-solidity/blob/master/ABDKMath64x64.sol#L687
+    function sqrt(uint256 x) internal pure returns (uint256) {
+        if (x == 0) return 0;
+        // this block is equivalent to r = uint256(1) << (BitMath.mostSignificantBit(x) / 2);
+        // however that code costs significantly more gas
+        uint256 xx = x;
+        uint256 r = 1;
+        if (xx >= 0x100000000000000000000000000000000) {
+            xx >>= 128;
+            r <<= 64;
         }
-        if (x >= 0x10000000000000000) {
-            x >>= 64;
-            r += 64;
+        if (xx >= 0x10000000000000000) {
+            xx >>= 64;
+            r <<= 32;
         }
-        if (x >= 0x100000000) {
-            x >>= 32;
-            r += 32;
+        if (xx >= 0x100000000) {
+            xx >>= 32;
+            r <<= 16;
         }
-        if (x >= 0x10000) {
-            x >>= 16;
-            r += 16;
+        if (xx >= 0x10000) {
+            xx >>= 16;
+            r <<= 8;
         }
-        if (x >= 0x100) {
-            x >>= 8;
-            r += 8;
+        if (xx >= 0x100) {
+            xx >>= 8;
+            r <<= 4;
         }
-        if (x >= 0x10) {
-            x >>= 4;
-            r += 4;
+        if (xx >= 0x10) {
+            xx >>= 4;
+            r <<= 2;
         }
-        if (x >= 0x4) {
-            x >>= 2;
-            r += 2;
+        if (xx >= 0x8) {
+            r <<= 1;
         }
-        if (x >= 0x2) r += 1;
-    }
-
-    // returns the 0 indexed position of the least significant bit of the input x
-    // s.t. (x & 2**lsb) != 0 and (x & (2**(lsb) - 1)) == 0)
-    // i.e. the bit at the index is set and the mask of all lower bits is 0
-    function leastSignificantBit(uint256 x) internal pure returns (uint8 r) {
-        require(x > 0, 'BitMath::leastSignificantBit: zero');
-
-        r = 255;
-        if (x & uint128(-1) > 0) {
-            r -= 128;
-        } else {
-            x >>= 128;
-        }
-        if (x & uint64(-1) > 0) {
-            r -= 64;
-        } else {
-            x >>= 64;
-        }
-        if (x & uint32(-1) > 0) {
-            r -= 32;
-        } else {
-            x >>= 32;
-        }
-        if (x & uint16(-1) > 0) {
-            r -= 16;
-        } else {
-            x >>= 16;
-        }
-        if (x & uint8(-1) > 0) {
-            r -= 8;
-        } else {
-            x >>= 8;
-        }
-        if (x & 0xf > 0) {
-            r -= 4;
-        } else {
-            x >>= 4;
-        }
-        if (x & 0x3 > 0) {
-            r -= 2;
-        } else {
-            x >>= 2;
-        }
-        if (x & 0x1 > 0) r -= 1;
+        r = (r + x / r) >> 1;
+        r = (r + x / r) >> 1;
+        r = (r + x / r) >> 1;
+        r = (r + x / r) >> 1;
+        r = (r + x / r) >> 1;
+        r = (r + x / r) >> 1;
+        r = (r + x / r) >> 1; // Seven iterations should be enough
+        uint256 r1 = x / r;
+        return (r < r1 ? r : r1);
     }
 }
